78c160391194cfded26f5a355a5753b2a186b5b0
gabruw <gabrielmarques.mg@gmail.com>
2024-07-09 08:30:56 -0300

    feat(main): applying the api provider and backdrop

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,28 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider } from "next-themes";
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "_styles/global/index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import { ApiProvider } from "_components/ApiProvider";
+import { Backdrop } from "_components/Backdrop";
 import { axeCoreAccessibilityReport } from "_configurations/axe-core/axeCoreAccessibilityReport";
 import { Router } from "_routes/router";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<NextUIProvider className="w-full h-full">
-			<ThemeProvider attribute="class">
-				<BrowserRouter>
-					<Router />
-				</BrowserRouter>
-			</ThemeProvider>
-		</NextUIProvider>
+		<Suspense fallback={<Backdrop />}>
+			<NextUIProvider className="w-full h-full">
+				<ThemeProvider attribute="class">
+					<ApiProvider>
+						<BrowserRouter>
+							<Router />
+						</BrowserRouter>
+					</ApiProvider>
+				</ThemeProvider>
+			</NextUIProvider>
+		</Suspense>
 	</StrictMode>,
 );
 
